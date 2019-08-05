17e7bbf59d3e04dc21a4ab2b34833ed9e70ff5d3
David <dav-ss@mail.ru>
2019-08-05 11:20:33 -0700

    Finishing app

---
+++ app/src/main/java/com/davidvardanyan/davfitness/DetailsActivity.java
@@ -0,0 +1,36 @@
+package com.davidvardanyan.davfitness;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+
+
+        TextView exerciseText = findViewById(R.id.exerciseTitle);
+        ImageView exerciseImage = findViewById(R.id.exerciseImage);
+        LinearLayout mainBG = findViewById(R.id.mainBG);
+
+        String exerciseTitle = getIntent().getStringExtra(MainActivity.EXTRA_ITEM_TITLE);
+        exerciseText.setText(exerciseTitle);
+
+        if (exerciseTitle.equalsIgnoreCase(MainActivity.EXERCISE_WEIGHTS)){
+            exerciseImage.setImageDrawable(getResources().getDrawable(R.drawable.weight,getApplicationContext().getTheme()));
+            mainBG.setBackgroundColor(Color.parseColor("#2ca5f5"));
+        } else if (exerciseTitle.equalsIgnoreCase(MainActivity.EXERCISE_YOGA)){
+            exerciseImage.setImageDrawable(getResources().getDrawable(R.drawable.lotus,getApplicationContext().getTheme()));
+            mainBG.setBackgroundColor(Color.parseColor("#916bcd"));
+        } else {
+            exerciseImage.setImageDrawable(getResources().getDrawable(R.drawable.heart,getApplicationContext().getTheme()));
+            mainBG.setBackgroundColor(Color.parseColor("#52ad56"));
+        }
+    }
+}
